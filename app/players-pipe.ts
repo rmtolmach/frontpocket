347235355d5b8bef0a0a7cff2ae328830495a69d
Rebecca Tolmach <rmtolmach@gmail.com>
2016-03-06 20:27:38 -0800

    nothing really. comments

--- app/players-pipe.ts
+++ app/players-pipe.ts
@@ -6,7 +6,9 @@ import {Pipe} from "angular2/core";
 
 export class PlayersPipe{
   transform(value, args:string[]){
-// if value exists, filter through all of the games, make an array of numbers (between the first number in the //range (parseInt(game.time_range) and the last numbers in the range) and check if the argument passed in from //the form (after being turned into an integer) is included in the array.
+// If value exists, filter through all of the games, make an array of numbers (between the first number in the 
+//range (parseInt(game.time_range) and the last numbers in the range) and check if the argument passed in from
+//the form (after being turned into an integer) is included in the array.
     if (value) {
       return value.filter((game)=>
         Array.apply(null, Array(parseInt(game.time_range.slice(-2)))).map(function (_, i) {return i + parseInt(game.time_range);}).includes(parseInt(args[0])));
--- app/time-pipe.ts
+++ app/time-pipe.ts
@@ -6,7 +6,10 @@ import {Pipe} from "angular2/core";
 
 export class TimePipe{
   transform(value, args:string[]){
-// if value exists, filter through all of the games, make an array of numbers (between the first number in the range (parseInt(game.time_range) and the last numbers in the range) and check if the argument passed in from the form (after being turned into an integer) is included in the array.
+// if value exists, filter through all of the games, make an array of numbers
+//(between the first number in the range (parseInt(game.time_range) and the last numbers
+//in the range) and check if the argument passed in from the form
+//(after being turned into an integer) is included in the array.
     if (value) {
       return value.filter((game)=>
         Array.apply(null, Array(parseInt(game.time_range.slice(-2)))).map(function (_, i) {return i + parseInt(game.time_range);}).includes(parseInt(args[0])));
