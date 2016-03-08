0cfb20ccc1bc550aea6b82abbd352c1ce2cd50c3
Rebecca Tolmach <rmtolmach@gmail.com>
2016-03-08 09:51:56 -0800

    nothing, i think

--- app/players-pipe.ts
+++ app/players-pipe.ts
@@ -6,10 +6,13 @@ import {Pipe} from "angular2/core";
 
 export class PlayersPipe{
   transform(value, args:string[]){
-// If value exists, filter through all of the games, make an array of numbers (between the first number in the 
+// If value exists, filter through all of the games, make an array of numbers (between the first number in the
 //range (parseInt(game.time_range) and the last numbers in the range) and check if the argument passed in from
 //the form (after being turned into an integer) is included in the array.
     if (value) {
+        if (args[0] === "whatever"){
+          //Don't apply the filter. Is this possible?
+        }
       return value.filter((game)=>
         Array.apply(null, Array(parseInt(game.time_range.slice(-2)))).map(function (_, i) {return i + parseInt(game.time_range);}).includes(parseInt(args[0])));
     }
--- app/time-pipe.ts
+++ app/time-pipe.ts
@@ -6,11 +6,16 @@ import {Pipe} from "angular2/core";
 
 export class TimePipe{
   transform(value, args:string[]){
+    console.log("these are the time args:" + args);
+
 // if value exists, filter through all of the games, make an array of numbers
 //(between the first number in the range (parseInt(game.time_range) and the last numbers
 //in the range) and check if the argument passed in from the form
 //(after being turned into an integer) is included in the array.
     if (value) {
+      if (args[0] === "doesntmatter"){
+        //Don't apply the filter. Is this possible?
+      }
       return value.filter((game)=>
         Array.apply(null, Array(parseInt(game.time_range.slice(-2)))).map(function (_, i) {return i + parseInt(game.time_range);}).includes(parseInt(args[0])));
     }
