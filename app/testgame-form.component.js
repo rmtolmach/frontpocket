e2cb959f6b862f371e45edabafca9d55126356f1
Rebecca Tolmach <rmtolmach@gmail.com>
2016-03-09 11:27:34 -0800

    made noise level more dynamic

--- app/testgame-form.component.js
+++ app/testgame-form.component.js
@@ -27,6 +27,7 @@ System.register(['angular2/core', 'angular2/common', 'angular2/router'], functio
             TestGameFormComponent = (function () {
                 function TestGameFormComponent(_router) {
                     this._router = _router;
+                    this.noise = ['Outside Voice', 'Inside Voice'];
                     this.equipment = ['none', 'ball (soft)', 'ball', 'boombox', 'hula-hoops'];
                     this.time = [5, 10, 15, 20, 25, 30];
                     this.players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50];
